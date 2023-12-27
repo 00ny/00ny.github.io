d06dd75842c4e6e38864a97085f7f0cdf812aa21
00ny <149352457+00ny@users.noreply.github.com>
2023-12-28 01:39:42 +0900

    4-2강까지의 진행
    특정 , 이벤트 리스너 , 이벤트 반응 , form input , window 기본 작업 정지시키기

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const loveme = document.querySelector(".love span");
-const love = document.querySelector(".love");
+const loginForm = document.getElementById("login-fform")
+// const loginForm = document.querySelector("#login-fform")
+// 이제 login-form이 선택된것이다.
+const loginInput = document.querySelector("#login-fform input:last-child");
+// const loginButton = loginForm.querySelector("button");
 
-loveme.innerText="love you too"
+const h = document.querySelector("h1");
 
-
-function handleclick() {
-    love.classList.toggle("active")
+function onLoginSubmit(event){
+    event.preventDefault();
+    // console.log(event);
+    const userInfo = document.getElementById("a").value;
+    loginInput.classList.toggle("hidden");
+    h.innerText=`hi ${userInfo}`; //`` vs ''
+    h.classList.toggle("hidden");
 }
 
-loveme.addEventListener("click", handleclick);
-// loveme.onclick = handleclick;
+// loginInput.addEventListener("submit", onLoginSubmit);
+// loginInput.addEventListener("enter", onLoginSubmit);
+loginInput.addEventListener("click", onLoginSubmit);
+
+// 왜 시 발 다 안되는건데?
+
+const link = document.querySelector("a")
+
+function clickLick(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+link.addEventListener("click", clickLick);
+
+// const memo = document.querySelector("h1")
+
+// memo.addEventListener("")
--- study/function.js
+++ study/function.js
@@ -22,3 +22,18 @@ const playver = {
 
 console.log(playver.name)
 playver.sayHello()
+
+//############
+
+const loveme = document.querySelector(".love span");
+const love = document.querySelector(".love");
+
+loveme.innerText="love you too"
+
+
+function handleclick() {
+    love.classList.toggle("active")
+}
+
+loveme.addEventListener("click", handleclick);
+// loveme.onclick = handleclick;

10a547047f3b280cef03ce61a779adbf6202eccf
00ny <149352457+00ny@users.noreply.github.com>
2024-01-08 19:17:08 +0900

    display size block
    화면 크기 변형을 방지

--- js/background.js
+++ js/background.js
@@ -1,11 +1,20 @@
 const images = ["js-002.png","js-001.png","js-002.png","js-000.png"];
+const body = document.querySelector("body");
 
-const chosenImage = images[Math.floor(Math.random()*images.length)];
+function change() {
+    console.log("you clicked!")
+    const chosenImage = images[Math.floor(Math.random()*images.length)];
+    console.log(chosenImage)
+    
+    const box_of_img = document.createElement("div");
+    const bgImage = document.createElement("img");
+    
+    bgImage.src = `img/${chosenImage}`;
+    
+    document.body.appendChild(box_of_img);
+    box_of_img.appendChild(bgImage);
+    box_of_img.classList.add("imgBox");
+}
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-console.log(bgImage)
-
-document.body.appendChild(bgImage);
+change();
+body.addEventListener("click",change)
